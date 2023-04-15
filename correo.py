3c9d63722c3f03be3ed95ed769ad31ee93f1c96e
luciacantos <luciacantos22@gmail.com>
2023-04-15 23:52:38 +0200

    bucle para comprobar el correo

--- correo.py
+++ correo.py
@@ -2,5 +2,19 @@ import re
 
 class Correo:
     def __init__(self):
-        self.correo = str("luciacantos@gmail.com")
+        self.correo_correcto = str("luciacantos@gmail.com")
+
+    def validar(self):
+        while True:
+            correo = input("Introduce tu correo electrónico: ")
+            if re.search(".*@.*\..*", correo):
+                if self.correo_correcto == correo:
+                    print("Bienvenido")
+                    break
+                else:
+                    print("Cuenta bloqueada a causa de un ataque")
+                    break
+            else:
+                print("Por favor, introduzca un formato correcto (xxx@xx.x)")
+                
 
