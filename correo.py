2e5ad628256553f7193afd3f3eeb3b6a2d3006bd
luciacantos <luciacantos22@gmail.com>
2023-04-16 12:44:49 +0200

    definir el usuario separando el correo antes de @

--- correo.py
+++ correo.py
@@ -7,15 +7,19 @@ class Correo:
     def validar(self):
         while True:
             correo = input("Introduce tu correo electrónico: ")
-            if re.search(".*@.*\..*", correo): # método para comprobar que los caracteres sean correctos
-                if self.correo_correcto == correo: # comprobar que las dos cadenas sean iguales
-                    print("Bienvenido")
-                    break
-                else:
-                    print("Cuenta bloqueada a causa de un ataque")
-                    break
+            if correo == "":
+                print("'' es una entrada incorrecta. Introduzca una dirección de correo.")
             else:
-                print("Por favor, introduzca un formato correcto (xxx@xx.x)")
+                if re.search(".*@.*\..*", correo): # método para comprobar que los caracteres sean correctos
+                    if self.correo_correcto == correo: # comprobar que las dos cadenas sean iguales
+                        usuario = correo.split("@")[0]
+                        print("Bienvenido " + usuario)
+                        break
+                    else:
+                        print("Cuenta bloqueada a causa de un ataque")
+                        break
+                else:
+                    print("Por favor, introduzca un formato correcto (xxx@xx.x)")
 
 
 Correo().validar() # al quitar print no devuelve el valor None
